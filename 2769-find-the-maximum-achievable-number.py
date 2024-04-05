b3177fd6a3f40eb4cc02b494a7318da4b6f23d3d
uk1618 <ukonak665@gmail.com>
2024-04-05 14:45:18 +0300

    2769. Question Solved

---
+++ 2769-find-the-maximum-achievable-number.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+        x = t
+        num = num + t + x
+        
+
+
+
+
+        return num
+
+solution = Solution()
+result = solution.theMaximumAchievableX(num=3, t=2)
+print(result)
